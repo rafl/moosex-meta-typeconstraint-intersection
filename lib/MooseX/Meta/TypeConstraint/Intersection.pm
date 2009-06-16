9eb079505c13e8f814dc5a005b6bc7baf6c9e3ca
Florian Ragwitz <rafl@debian.org>
2009-06-16 04:11:52 +0200

    Add documentation.

--- lib/MooseX/Meta/TypeConstraint/Intersection.pm
+++ lib/MooseX/Meta/TypeConstraint/Intersection.pm
@@ -1,4 +1,5 @@
 package MooseX::Meta::TypeConstraint::Intersection;
+# ABSTRACT: An intersection of Moose type constraints
 
 use Moose;
 use MooseX::Types::Moose qw/ArrayRef/;
@@ -6,7 +7,26 @@ use Moose::Util::TypeConstraints 'find_type_constraint';
 use aliased 'Moose::Meta::TypeConstraint';
 use namespace::autoclean;
 
+=head1 DESCRIPTION
+
+This class represents an intersection of type constraints. An intersection
+takes multiple type constraints, and is true if all of its member constraints
+are true.
+
+=head1 INHERITANCE
+
+C<MooseX::Meta::TypeConstraint::Intersection> is a subclass of
+L<Moose::Meta::TypeConstraint>.
+
+=cut
+
 extends TypeConstraint;
+
+=attr type_constraints
+
+The member type constraints of this intersection.
+
+=cut
 
 has type_constraints => (
     is      => 'ro',
@@ -14,12 +34,32 @@ has type_constraints => (
     default => sub { [] },
 );
 
+=method new(%options)
+
+This creates a new intersection type constraint based on the given C<%options>.
+
+It takes the same options as its parent. It also requires an additional option,
+C<type_constraints>. This is an array reference containing the
+L<Moose::Meta::TypeConstraint> objects that are the members of the intersection
+type. The C<name> option defaults to the names of all of these member types
+sorted and then joined by an ampersand (&).
+
+=cut
+
 around new => sub {
     my ($next, $class, %args) = @_;
     my $name = join '&' => sort { $a cmp $b }
         map { $_->name } @{ $args{type_constraints} };
     return $class->$next(name => $name, %args);
 };
+
+=method check($value)
+
+Checks a C<$value> against the intersection constraint. If all member
+constraints accept the value, the value is valid and something true is
+returned.
+
+=cut
 
 sub _actually_compile_type_constraint {
     my ($self) = @_;
@@ -34,6 +74,13 @@ sub _actually_compile_type_constraint {
         return 1;
     };
 }
+
+=method equals($other_constraint)
+
+A type is considered equal if it is also an intersection type, and the two
+intersections have the same member types.
+
+=cut
 
 # this is stolen from TC::Union. meh
 sub equals {
@@ -59,11 +106,24 @@ sub equals {
     return @other_constraints == 0;
 }
 
+=method parents
+
+This returns the same constraint as the C<type_constraints> method.
+
+=cut
+
 # this too, although i'm not too sure what the point of it is
 sub parents {
     my ($self) = @_;
     return $self->type_constraints;
 }
+
+=method validate($value)
+
+Like C<check>, but returns an error message including all of the error messages
+returned by the member constraints, or C<undef>.
+
+=cut
 
 sub validate {
     my ($self, $value) = @_;
@@ -79,6 +139,14 @@ sub validate {
         ? $msg . ' in ' . $self->name
         : undef;
 }
+
+=method is_subtype_of($other_constraint)
+
+This returns true if the C<$other_constraint> is also an intersection
+constraint and contains at least all of the member constraints of the
+intersection this method is called on.
+
+=cut
 
 sub is_subtype_of {
     my ($self, $type_or_name) = @_;
